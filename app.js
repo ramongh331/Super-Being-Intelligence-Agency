199e92cc4820982d5f2f1ec4128078510926706f
Ramon Hernandez <ramongh331@gmail.com>
2022-10-22 20:33:12 -0400

    HTML&JS: Functionality Check / CSS: Started Progress Bar

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ const baseURL = "https://superheroapi.com/api.php/";
 const $heroName = $("#heroName");
 const $heroID = $("#heroID");
 const $bio = $("#bio");
+const $realName = $("#real-name");
+const $alterEgo = $("#alter-ego");
+const $alias = $("#alias");
+const $birthPlace = $("#birth-place");
+const $appearance = $("#appearance");
+const $publisher = $("#publisher");
+const $alignment = $("#alignment");
+const $gender = $("#gender");
+const $race = $("#race");
+const $weight = $("#weight");
+const $eyeColor = $("#eye-color");
+const $hairColor = $("#hair-color");
+const $intelligence = $("#intelligence");
+const $strength = $("#strength");
+const $speed = $("#speed");
+const $durability = $("#durability");
+const $power = $("#power");
+const $combat = $("#combat");
+const $image = $("#hero-pic");
+
 const $input = $("#herosDropDown");
 
 $input.on("change", handleGetData);
@@ -20,6 +40,7 @@ function handleGetData(event) {
   }).then(
     (data) => {
       superHeroData = data;
+      // console.log(data);
       render();
     },
     (error) => {
@@ -27,17 +48,34 @@ function handleGetData(event) {
     }
   );
 }
-// handleGetData();
 
 function render() {
-  $heroName.text(`Name: ` + superHeroData.name);
-  $heroID.text(`Id: ` + superHeroData.id);
-  $bio.text(`Bio: ` + superHeroData.biography.full - name); //ask how to get rid of name being crossed out.
+  $heroName.text(superHeroData.name);
+  $heroID.text(superHeroData.id);
+  $realName.text(superHeroData.biography["full-name"]);
+  $alterEgo.text(superHeroData.biography["alter-egos"]);
+  $alias.text(superHeroData.biography.aliases.join(", "));
+  $birthPlace.text(superHeroData.biography["place-of-birth"]);
+  $appearance.text(superHeroData.biography["first-appearance"]);
+  $publisher.text(superHeroData.biography["publisher"]);
+  $alignment.text(superHeroData.biography.alignment);
+  $gender.text(superHeroData.appearance.gender);
+  $race.text(superHeroData.appearance.height[0]);
+  $weight.text(superHeroData.appearance.weight[0]);
+  $eyeColor.text(superHeroData.appearance["eye-color"]);
+  $hairColor.text(superHeroData.appearance["hair-color"]);
+  $intelligence.text(superHeroData.powerstats.intelligence);
+  $strength.text(superHeroData.powerstats.strength);
+  $speed.text(superHeroData.powerstats.speed);
+  $durability.text(superHeroData.powerstats.durability);
+  $power.text(superHeroData.powerstats.power);
+  $combat.text(superHeroData.powerstats.combat);
+  $image.html(`<img src="${superHeroData.image.url}">`);
+
+  // change element style in javascript
+  const $intelligenceProgress = $("#intelligenceProgress");
+  $intelligenceProgress.css(
+    "width",
+    superHeroData.powerstats.intelligence + "%"
+  );
 }
-
-// function displayVals() {
-//   var singleValues = $("#herosDropDown").val();
-//   console.log(singleValues);
-// }
-
-// displayVals();
